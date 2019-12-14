e81110f463675c34c73dc24232bce4936a3231e3
Bohdan Hereha <bogdangerega19@gmail.com>
2019-12-14 14:06:49 +0200

    Update js file

--- lab3.js
+++ lab3.js
@@ -15,30 +15,29 @@ if (process.argv.length <= 3) {
 };
 
 function WordCount(data, prevData, result) {
-    data = data.replace(/[^A-Za-zА-Яа-я\s+]/g,'');
+    data = data.replace(/[^A-Za-zА-Яа-я\s+]/g, "");
+    var res =0;
 
     if (data == 0) {
         return 0;
     }
-    var res = data.split(/\s+/g).length;
-    
-    if (data[0] == " " || data[0] == "\n" || data[0] == 0) {
-            res--;
-    }
-
-    if (data[data.length - 1] == " " || data[data.length - 1] == "\n" || data[data.length - 1] == 0) {
-            res--;
-    }
 
     if (result !== 0) {
         if (prevData.length !== 0) {
-			if (prevData[prevData.length - 1] != " " && data[0] != " ") {
-                if (prevData[prevData.length - 1] != "\n" && data[0] != "\n") {
+            var prev = prevData[prevData.length - 1];
+            var next = data[0];
+	    if (prev !== " "  &&
+                prev !== "\n" &&
+                prev !== "\r" &&
+                next !== " "  && 
+                next !== "\n" && 
+                next !== "\r") {
 	            res--;
-       	        }
             }
-		}
+	}
     }
+    res += data.trim().split(/\s+/g).length;
+
     return res;
 };
 
@@ -50,41 +49,41 @@ function makeFolder(path) {
     } catch (err) {
         throw err;
     }
-};
+}
 
 function mainWordCount(dir1, dir2) {
     makeFolder(dir2);
 
     let files = fs.readdirSync(dir1)
         .filter(fileName => fileName.endsWith('.txt'));
-
     let counter = 0;
     for (let file of files) {
         let prevData = '';
         let prevResult = 0;
         let result = 0;
-        const stream = fs.createReadStream(dir1 + '/' + file, { highWaterMark: 100})
+        const stream = fs.createReadStream(dir1 + '/' + file, { highWaterMark: 1})
         .on('data', (data) => {
             data = data.toString();
-			result += WordCount(data, prevData, result);
-                
+	    result += WordCount(data, prevData, result);
+            prevData = data;
+
             const resFile = dir2 + '/' + file.substring(0, file.indexOf('.txt')) + '.res';
             if (result)
-				fs.writeFile(resFile, result, (err) => {
-				if (err) throw err;
-            });
-			prevData = data;
-		})
-		.on('end', () => {
-			counter++;
-			if (counter == files.length)
-				console.log('Total number of processed files: ' + files.length);
-		});
-	}		
-};
+                fs.writeFile(resFile, result, (err) => {
+                if (err) throw err;
+                });
+        })
+
+        .on('end', () => {
+            counter++;
+            if (counter == files.length)
+                console.log('Total number of processed files: ' + files.length);
+        });
+    }
+}
 
 if (!process.argv[2] || !process.argv[3]) {
-	throw new Error('Incorrect input!');
+    throw new Error('Incorrect input!');
 } else {
-	mainWordCount(process.argv[2], process.argv[3]);
-};
+    mainWordCount(process.argv[2], process.argv[3]);
+}
